0c054d9e7340fe3a417d91c856278419b0968717
Ry <ry.e.desoto@gmail.com>
2019-01-20 21:42:11 -0600

    Updates

---
+++ precache-manifest.f108ea00f517071f3bca6596bc62944e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "aa6e3e95d62994145ebe",
+    "url": "/clickygame/static/css/main.785029e3.chunk.css"
+  },
+  {
+    "revision": "aa6e3e95d62994145ebe",
+    "url": "/clickygame/static/js/main.aa6e3e95.chunk.js"
+  },
+  {
+    "revision": "9c49b1657f794805e3dd",
+    "url": "/clickygame/static/js/1.9c49b165.chunk.js"
+  },
+  {
+    "revision": "a70c675f9a23daa38fda",
+    "url": "/clickygame/static/js/runtime~main.a70c675f.js"
+  },
+  {
+    "revision": "bc33ceec630cdf2032d4e6f5088d9d4e",
+    "url": "/clickygame/index.html"
+  }
+];
